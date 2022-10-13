fef06e41cc81283646464203e024988a4f61ddcd
LJNERO8 <ljnero8@gmail.com>
2022-10-14 01:11:11 +0300

    "task 4 implemented"

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import builtins
+
 print("\r\n Part 1.\n")
 
 int_a = 55
@@ -25,3 +27,24 @@ print(f"Type of str_b is: {type(str_b)}")
 print(f"Type of set_c is: {type(set_c)}")
 print(f"Type of lst_d is: {type(lst_d)}")
 print(f"Type of dict_e is: {type(dict_e)}")
+
+print(isinstance(int_a, str))
+
+print("\r\n Part 4.\n")
+
+# we get the list of all types available in Python and place them in a list
+builtin_types = [getattr(builtins, i) for i in dir(builtins) if isinstance(getattr(builtins, i), type)]
+# since all of the variable types are also objects we remove the type 'object' from the list to leave only exact types
+builtin_types.remove(builtins.object)
+# next we check with isinstance each of our variables in a cycle to determine which type they are
+for i in builtin_types:
+    if isinstance(int_a, i):
+        print(f"int_a is a type of: {i} \r")
+    if isinstance(str_b, i):
+        print(f"str_b is a type of: {i} \r")
+    if isinstance(set_c, i):
+        print(f"set_c is a type of: {i} \r")
+    if isinstance(lst_d, i):
+        print(f"lst_d is a type of: {i} \r")
+    if isinstance(dict_e, i):
+        print(f"dict_e is a type of: {i} \r")
