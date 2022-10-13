085e61f24511ad4e9f03cbb048f60556ce863f0a
LJNERO8 <ljnero8@gmail.com>
2022-10-14 00:14:34 +0300

    "Defined the ids of next variables: int_a, str_b, set_c, lst_d, dict_e"

---
+++ main.py
@@ -0,0 +1,11 @@
+int_a = 55
+str_b = 'cursor'
+set_c = {1, 2, 3}
+lst_d = [1, 2, 3]
+dict_e = {'a': 1, 'b': 2, 'c': 3}
+
+print(f"ID of int_a is{id(int_a)} and value is {int_a}")
+print(f"ID of str_b is{id(str_b)} and value is {str_b}")
+print(f"ID of set_c is{id(set_c)} and value is {set_c}")
+print(f"ID of lst_d is{id(lst_d)} and value is {lst_d}")
+print(f"ID of dict_e is{id(dict_e)} and value is {dict_e}")
