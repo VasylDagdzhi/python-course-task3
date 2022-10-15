957039efabec06b20af9d098d016f1f77ea78602
LJNERO8 <ljnero8@gmail.com>
2022-10-15 22:55:16 +0300

    Task 26 implemented

--- main.py
+++ main.py
@@ -230,5 +230,6 @@ print(b_negative)
 list_1 = [1, 2, 3, 5, 7, 9]
 list_2 = [2, 3, 5, 6, 7, 8]
 print("\r\n Part 26.\n")
-
+lst_common = list(filter(lambda x: x in list_2, list_1))
+print(lst_common)
 
